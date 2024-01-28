4424c7a76988068bfa3f993c5da5ae472293c948
Roman Braverman <romanbr87@gmail.com>
2024-01-28 04:32:38 +0200

    update

--- app.js
+++ app.js
@@ -59,7 +59,9 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://byzbook1.vercel.app');
+  
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Credentials', 'true');
